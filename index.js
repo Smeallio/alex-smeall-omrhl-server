49a94716f6a37fd7769e0ee53e8d3b37013596e5
Smeallio <alexsmeall@gmail.com>
2024-02-05 17:17:11 -0330

    Revert API URL back to localhost

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use((_req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://oddmanrushhockey.netlify.app');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
