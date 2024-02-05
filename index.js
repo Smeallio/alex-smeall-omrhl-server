0a5d83c1f68a1242cd01f5e046188b0e2c558eb1
Smeallio <alexsmeall@gmail.com>
2024-02-05 15:47:10 -0330

    Change up cors headers

--- index.js
+++ index.js
@@ -16,11 +16,12 @@ app.get("/", (req, res) => {
 app.use(cors());
 app.use(express.json());
 
-const corsOptions = {
-  origin: "https://oddmanrushhockey.netlify.app/"
-};
-
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // or specific origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use("/api/users", userRoutes);
 app.use("/api/teams", teamRoutes);
