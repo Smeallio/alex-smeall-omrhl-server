179c49083dc8686308f42a43408281fe1328266d
Smeallio <alexsmeall@gmail.com>
2024-02-16 15:59:37 -0330

    Fix API error with getPlayersByTeam call

--- controllers/stat-controllers.js
+++ controllers/stat-controllers.js
@@ -11,7 +11,7 @@ const getAllSkaterStats = async (_req, res) => {
 
 const getSkaterStatsByGame = async (req, res) => {
   try {
-    const skaterStatsByGame = await knex("skaterStats").where({ id: req.params.gameId }); // Sync syntax for other controller
+    const skaterStatsByGame = await knex("skaterStats").where({ game_id: req.params.gameId }); // Sync syntax for other controller
     if (!skaterStatsByGame) {
       return res.status(404).json({ error: "Game not found" });
     }
@@ -21,7 +21,24 @@ const getSkaterStatsByGame = async (req, res) => {
   }
 };
 
+const addSkaterStats = async (req, res) => {
+    if (!req.body.game_id || !req.body.player_id || !req.body.team_id ) {
+      return res.status(400).json({
+        message: "Invalid game - game id, player id or team id",
+      });
+    }
+    try {
+      const result = await knex("skaterStats").insert(req.body);
+      res.status(201).json(result);
+    } catch (err) {
+      res.status(500).json({
+        message: `Unable to add stats due to: ${err}`,
+      });
+    }
+  };
+
 module.exports = {
   getAllSkaterStats,
   getSkaterStatsByGame,
+  addSkaterStats
 };
--- routes/stat-routes.js
+++ routes/stat-routes.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const statController = require("../controllers/stat-controllers");
 
-router.route("/skaters").get(statController.getAllSkaterStats)
+router.route("/skaters").get(statController.getAllSkaterStats).post(statController.addSkaterStats)
 
 router
-  .route("/:gameId")
+  .route("/skaters/:gameId")
   .get(statController.getSkaterStatsByGame)
 
 module.exports = router;
