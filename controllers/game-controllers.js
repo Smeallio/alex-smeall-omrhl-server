f63d6dd7108cea00cbc12160c0ea59b2e3adfa7c
Smeallio <alexsmeall@gmail.com>
2024-02-19 01:36:16 -0330

    Update getAllGames logic to sort by date and time

--- controllers/game-controllers.js
+++ controllers/game-controllers.js
@@ -3,6 +3,19 @@ const knex = require("knex")(require("../knexfile"));
 const getAllGames = async (_req, res) => {
   try {
     const games = await knex("games");
+
+    games.sort((a,b) => {
+      const dateA = new Date(a.date + " " + a.time);
+      const dateB = new Date(b.date + " " + b.time);
+
+      if (dateA < dateB) return -1;
+      if (dateA > dateB) return 1;
+
+      const timeA = parseInt(a.time.split(":")[0]);
+      const timeB = parseInt(b.time.split(":")[0]);
+      return timeA - timeB;
+    })
+
     res.status(200).json(games);
   } catch (err) {
     res.status(500).send(`Error retrieving games from the database: ${err}`);
