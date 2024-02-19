e435082fdef94bcfd4b65f5761e62b1831475ed1
Smeallio <alexsmeall@gmail.com>
2024-02-19 02:22:35 -0330

    Change order of the games routes

--- routes/game-routes.js
+++ routes/game-routes.js
@@ -1,19 +1,14 @@
 const router = require("express").Router();
 const gameController = require("../controllers/game-controllers");
 
-router
-  .route("/")
-  .get(gameController.getAllGames)
-  .post(gameController.addGame);
+router.route("/").get(gameController.getAllGames).post(gameController.addGame);
+
+router.route("/standings").get(gameController.getStandings);
 
 router
   .route("/:gameId")
   .get(gameController.getOneGame)
   .put(gameController.updateGame)
   .delete(gameController.deleteGame);
-
-router
-  .route("/standings")
-  .get(gameController.getStandings)
 
 module.exports = router;
