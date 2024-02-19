c6cd37cd5442d58f634004bb3cac89b730a8d343
Smeallio <alexsmeall@gmail.com>
2024-02-19 01:51:30 -0330

    Final fix to getAllGames sorting logic

--- controllers/game-controllers.js
+++ controllers/game-controllers.js
@@ -5,9 +5,15 @@ const getAllGames = async (_req, res) => {
     const games = await knex("games");
 
     games.sort((a,b) => {
-      const dateA = new Date(a.date + "T" + a.time);
-      const dateB = new Date(b.date + "T" + b.time);
-      return dateA - dateB;
+      const dateA = new Date(a.date);
+      const dateB = new Date(b.date);
+      
+      if (dateA < dateB) return -1;
+      if (dateA > dateB) return 1;
+
+      const timeA = parseInt(a.time.split(":")[0]);
+      const timeB = parseInt(b.time.split(":")[0]);
+      return timeA - timeB;
     })
 
     res.status(200).json(games);
