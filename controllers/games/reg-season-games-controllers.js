3c93aad8d40067a5d5d44941194e9fad8cfc23da
Smeallio <alexsmeall@gmail.com>
2025-01-16 23:39:21 -0330

    Fix knex file path typo

--- controllers/games/reg-season-games-controllers.js
+++ controllers/games/reg-season-games-controllers.js
@@ -1,4 +1,4 @@
-const knex = require("knex")(require("../knexfile"));
+const knex = require("knex")(require("../../knexfile"));
 
 const getAllRegSeasonGames = async (_req, res) => {
   try {
--- routes/standings-routes.js
+++ routes/standings-routes.js
@@ -2,3 +2,5 @@ const router = require("express").Router();
 const standingsController = require("../controllers/standings/standings-controllers");
 
 router.route("/:seasonYear").get(standingsController.getStandings);
+
+module.exports = router;
